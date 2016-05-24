db95296c3ddc3dbd676c48b0a9b71c4946c920ad
Grace <grace.h.gamble@gmail.com>
2016-05-24 15:59:59 +0000

    Final JS2 String Reporter Copy

--- JS2.js
+++ JS2.js
@@ -2,3 +2,8 @@ var a = prompt("Enter a word!");
 alert("You entered:" + " " + a);
 
 alert("There are" + " " + a.length + " " + "characters in the word.");
+alert("The third character is n.");
+alert(a.toLowerCase());
+alert(a.toUpperCase());
+alert(a.substring(1, 4));
+alert("One of may favorite fruits is a" + " " + a + " " + "because they are so juicy.")
